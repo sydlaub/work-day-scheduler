aa59f9891b4fed6876ec71b2e8f00ea5b2adebd0
Sydney Laub <syd.laub18@gmail.com>
2023-05-02 10:56:42 -0400

    edited HTML to add time blocks for each hour of the work day

--- Develop/script.js
+++ Develop/script.js
@@ -11,23 +11,46 @@ $(function () {
   //
 
 
- $("#saveBtn").click(function(){
-  const timeBlock = $(this).closest('.time-block');
-  const timeBlockId = timeBlock.attr('id');
-  const hourX = timeBlock.find('#hour-x');
-  const userInput = $(this).prev().val;
-  localStorage.setItem(timeBlockId + "-" + hourX.text(), userInput);
-  }
-  )
+ $(".saveBtn").on("click", function(){
+  // get id and description from the nearest time-block div
+  var id = $(this).closest('.time-block').attr('id');
+  var description = $(this).siblings(".description").val();
+  // Save description to local storage
+  localStorage.setItem(id, description)
+  });
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+
+  // loop through each time-block div
+
+  $('.time-block').each(function(){
+    // get the hour from the div's id
+    var blockHour = parseInt($(this).attr("id"));
+    
+    // add or remove past, present and future classes based on current hour
+    if (blockHour < currentDate) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (blockHour === currentDate) {
+      $(this).removeClass("past future").addClass("present");
+    } else (blockHour > currentDate) {
+      $(this).removeClass("past present").addClass("future");
+    }
+    });
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  function currentDate() {
+    // get current date
+    var currentDate = moment().hour()
+  }
 });
