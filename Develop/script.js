be2c577c42428f05c4e682a0d111f85804675606
Sydney Laub <syd.laub18@gmail.com>
2023-05-02 15:00:33 -0400

    bug fix for displaying saved time block descriptions on the calendar on page refresh and updates to Readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,58 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+const savedNotesArray = JSON.parse(localStorage.getItem("savedNotes")) || []
+
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  //code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements
+  $(savedNotesArray).each(function (index, item) {
+    $(`div #${item.id}`).children('textarea').text(`${item.description}`);
+  })
 
+  //listener for click events on the save button
+  $(".saveBtn").on("click", function () {
+    // get id and description from the nearest time-block div
+    var id = $(this).parent('.time-block').attr('id');
+    var description = $(this).siblings(".description").val();
+    const obj = {
+      id: id,
+      description: description
+    }
 
- $(".saveBtn").on("click", function(){
-  // get id and description from the nearest time-block div
-  var id = $(this).siblings('.time-block').attr('id');
-  var description = $(this).siblings(".description").val();
-  // Save description to local storage
-  localStorage.setItem(id, description)
+    // here is where you would check to see if your savedNotesArray has
+    // an item with id value of your var id
+    // if the array has an item with id value = var id, then splice the item from the array, passing in the index you found with .findIndex() method as the first argument,second argument is 1, and then the i
+    // const theIndex = savedNotesArray.findIndex();
+    // if theIndex returns -1, then don't do anything else on this
+    // otherwise:
+    // savedNotesArray.splice(theIndex, 1)
+    
+    savedNotesArray.push(obj)
+    // Save description to local storage
+    localStorage.setItem("savedNotes", JSON.stringify(savedNotesArray))
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
-
+  //apply the past, present, or future class to each time block by comparing the id to the current hour:
   // loop through each time-block div
   function updateHour() {
     // get current time using js
     var currentHour = moment().hour();
 
-  // loop through each time-block div
-  $('.time-block').each(function(){
-    // get the hour from the div's id
-    var blockHour = parseInt($(this).attr("id"));
-    
-    // add or remove past, present and future classes based on current hour
-    if (blockHour < currentHour) {
-      $(this).removeClass("present future").addClass("past");
-    } else if (blockHour === currentHour) {
-      $(this).removeClass("past future").addClass("present");
-    } else {
-      $(this).removeClass("past present").addClass("future");
-    }
+    // loop through each time-block div
+    $('.time-block').each(function () {
+      // get the hour from the div's id
+      var blockHour = parseInt($(this).attr("id"));
+
+      // add or remove past, present and future classes based on current hour
+      if (blockHour < currentHour) {
+        $(this).removeClass("present future").addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past future").addClass("present");
+      } else {
+        $(this).removeClass("past present").addClass("future");
+      }
     });
 
 
@@ -52,19 +61,18 @@ $(document).ready(function () {
   setInterval(updateHour, 1000);
 
 
-  //code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements
-$(".time-block").each(function(){
-  var id = $(this).attr("id");
-  var description = localStorage.getItem(id);
-  if (description !== null) {
-    $(this).children(".description").val(description);
-  }
-})
+
+  // var id = $(this).attr("id");
+  // var description = localStorage.getItem(id);
+  // if (description !== null) {
+  //   $(this).children(".description").val(description);
+  // }
 
 
 
-//Add code to display the current date in the header of the page.
-var currentDay = moment().format("dddd, MMMM Do");
-$("#currentDay").text(currentDay);
+
+  //Add code to display the current date in the header of the page.
+  var currentDay = moment().format("dddd, MMMM Do");
+  $("#currentDay").text(currentDay);
 
 });
---
+++ README.md
@@ -0,0 +1,18 @@
+# Password Generator
+
+## Description
+The purpose of this application is to allow a user to add and save items to a daily planner with the visual aid of the hours of the day being color coordinated to reflect hours that have passed (white), the current hour (red) and the future hours (green). The application is designed to store saved items into local storage so that the user will be able to see their saved events even when the page is closed and then re-opened.  
+
+## Installation
+
+## Usage
+Links:
+- [Link to deployed site](https://sydlaub.github.io/work-day-scheduler/)
+- [Link to github repository](https://github.com/sydlaub/work-day-scheduler)
+
+The website can be viewed on a desktop web browser as shown below
+![Screenshot of deployed Password Generator site]()
+
+## Credits
+Collaborators include teachers, TAs, tutors and fellow students in the Columbia University Full Stack Engineering Bootcamp 2023 Spring cohort.
+NOTE: This application starter code was provided by the Columbia University School of Engineering.  
